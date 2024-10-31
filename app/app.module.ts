257312f4c21925876ae414aa10f3b2fd314f5351
Mamtesh2001 <mamteshbagade2001@gmail.com>
2024-10-31 21:48:52 +0530

    Initial commit

---
+++ app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { CreateClientComponent } from './create-client/create-client.component';
+import { ClientmeetingsComponent } from './clientmeetings/clientmeetings.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+const routes:Routes=[
+  {path:'',redirectTo:'login',pathMatch:'full'},
+  {path:'create',component:CreateClientComponent},
+  {path:'login',component:LoginComponent},
+  {path:'clientmeeting',component:ClientmeetingsComponent},
+]
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    CreateClientComponent,
+    ClientmeetingsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule
+
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
